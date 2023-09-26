93e5ada21accc9c589c955f4c23c2955a4a468a6
Yosuke Ota <otameshiyo23@gmail.com>
2023-09-26 17:54:36 +0900

    fix: add uroborosql-fmt-napi-0.0.0.tgz  to .vscodeignore (#7)
    * fix: add uroborosql-fmt-napi-0.0.0.tgz  to .vscodeignore
    
    * fix

---
+++ server/download-uroborosql-fmt-napi.mjs
@@ -0,0 +1,16 @@
+import { writeFileSync } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+main();
+
+async function main() {
+  const res = await fetch(
+    "https://future-architect.github.io/uroborosql-fmt/uroborosql-fmt-napi-0.0.0.tgz"
+  );
+  const destination = join(__dirname, "uroborosql-fmt-napi-0.0.0.tgz");
+  writeFileSync(destination, Buffer.from(await res.arrayBuffer()));
+}
