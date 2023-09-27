0c094665b4397a0f27b3970484195e4748088468
Yosuke Ota <otameshiyo23@gmail.com>
2023-09-27 13:39:40 +0900

    chore: support for proxy to download-uroborosql-fmt-napi (#10)

--- server/download-uroborosql-fmt-napi.mjs
+++ server/download-uroborosql-fmt-napi.mjs
@@ -1,6 +1,7 @@
 import { writeFileSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
+import { ProxyAgent } from "undici";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,9 +9,38 @@ const __dirname = dirname(__filename);
 main();
 
 async function main() {
+  const agent = autoProxyAgent();
   const res = await fetch(
-    "https://future-architect.github.io/uroborosql-fmt/uroborosql-fmt-napi-0.0.0.tgz"
+    "https://future-architect.github.io/uroborosql-fmt/uroborosql-fmt-napi-0.0.0.tgz",
+    agent ? { dispatcher: agent } : undefined
   );
   const destination = join(__dirname, "uroborosql-fmt-napi-0.0.0.tgz");
   writeFileSync(destination, Buffer.from(await res.arrayBuffer()));
+}
+
+function autoProxyAgent() {
+  const PROXY_ENV = [
+    "https_proxy",
+    "HTTPS_PROXY",
+    "http_proxy",
+    "HTTP_PROXY",
+    "npm_config_https_proxy",
+    "npm_config_http_proxy",
+  ];
+
+  const proxyStr = PROXY_ENV.map((k) => process.env[k]).find((v) => v);
+  if (!proxyStr) {
+    return null;
+  }
+  const proxyUrl = new URL(proxyStr);
+
+  return new ProxyAgent({
+    uri: proxyUrl.protocol + proxyUrl.host,
+    token:
+      proxyUrl.username || proxyUrl.password
+        ? `Basic ${Buffer.from(
+            `${proxyUrl.username}:${proxyUrl.password}`
+          ).toString("base64")}`
+        : undefined,
+  });
 }
